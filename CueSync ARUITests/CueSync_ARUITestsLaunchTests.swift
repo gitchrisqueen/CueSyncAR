1d9b76edf2d69f2feff474a173a6947003bd755c
Christopher Queen <christopher.queen@gmail.com>
2023-10-17 14:00:27 -0400

    Initial Commit

---
+++ CueSync ARUITests/CueSync_ARUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CueSync_ARUITestsLaunchTests.swift
+//  CueSync ARUITests
+//
+//  Created by Christopher Queen on 10/17/23.
+//
+
+import XCTest
+
+final class CueSync_ARUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
